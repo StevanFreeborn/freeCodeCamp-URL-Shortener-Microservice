017ef575fd005fb19b4de5a3b65e5b98388e26be
StevanFreeborn <sfreeborn7@gmail.com>
2022-05-05 15:02:54 -0500

    begin implementing api endpoints

--- server.js
+++ server.js
@@ -1,21 +1,73 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require('mongodb');
 
-app.use(cors());
+const EXPRESS = require('express');
+const CORS = require('cors');
+const MONGOOSE = require('mongoose');
+const BODYPARSER = require('body-parser');
+const URL = require('url');
+const DNS = require('dns');
 
-app.use(express.static(`public`));
+const APP = EXPRESS();
 
-app.get('/', function(req, res) {
+// enable cors
+APP.use(CORS());
+
+// serve static files from public directory
+APP.use(EXPRESS.static(`public`));
+
+// middleware parses requests where content-type
+// header is urlencoded and populates req.body
+// property with parsed body. The extended option
+// set to false parses using the querystring library.
+APP.use(BODYPARSER.urlencoded({extended: false}));
+
+// connect to mongodb
+MONGOOSE.connect(
+  process.env.MONGO_URI,
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  }
+);
+
+// build url schema
+const Schema = MONGOOSE.Schema;
+
+const urlSchema = new Schema({
+  original_url: {type: String, required: true},
+  short_url: {type: String, required: true}
+});
+
+APP.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// test route
-app.get('/api/greeting', function(req, res) {
-  res.json({ greeting: 'hello API' });
+APP.post('/api/shorturl/', (req, res) => {
+  let url = URL.parse(req.body.url, false);
+
+  DNS.lookup(url.hostname, {all: true} , (err, addresses) => {
+    if(err || addresses.length == 0) {
+      return res.json({
+        error: 'invalid url'
+      });
+    }
+    
+    // TODO: Create new short url
+    // TODO: Save new short url to database
+    // TODO: Return new short url as json object
+
+    res.json({message: 'here'});
+  });
 });
 
-const listener = app.listen(process.env.PORT || 3000, function() {
+APP.get('/api/shorturl/:shortUrl', (req, res) => {
+  
+  let shortUrl = req.params.shortUrl;
+  
+  res.send(shortUrl);
+});
+
+const listener = APP.listen(process.env.PORT || 3000, () => {
     console.log('Your app is listening on port ' + listener.address().port);
 });

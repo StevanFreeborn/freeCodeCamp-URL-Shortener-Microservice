aefbbb77e6171fad4e99e5a395ef96443124965b
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-10-18 00:24:16 +0000

    fix: dynamically build links referenced in index view.

---
+++ public/index.js
@@ -0,0 +1,7 @@
+document.getElementById('baseUrl').innerText = window.location.href;
+
+document.getElementById('examplePostRequestUrl').innerText =
+  window.location.href + 'api/shorturl';
+
+document.getElementById('exampleGetRequestUrl').innerText =
+  window.location.href + 'api/shorturl/5HnSOll6pcVs';
